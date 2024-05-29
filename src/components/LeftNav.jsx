55f22252932c4e4ebb2c50c3af732cda7bd31a25
Anthony Salinas Suarez <anthonysalinas78251@berkeley.edu>
2024-05-28 18:25:08 -0700

    Changed background of spotify embed

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -65,12 +65,13 @@ function LeftNav() {
         </div>
         <div className = "spotify-container">
           <iframe
-            style={{ borderRadius: '12px'}}
-            src="https://open.spotify.com/embed/playlist/3gkTnIG2dHkhcBCnxgcDc1?utm_source=generator"
+            style={{ borderRadius: '12px' }}
+            src="https://open.spotify.com/embed/playlist/3gkTnIG2dHkhcBCnxgcDc1?utm_source=generator&theme=0"
             frameBorder="0"
             allowFullScreen
             allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
             loading="lazy"
+            title="Spotify Playlist"
             className = "spotify-playlist"
           ></iframe>
         </div>
